7b5f459cd7d62d141c1775fe75178ab9a2f60ae2
koki <koki@KokisMacBookPro.local>
2023-03-03 22:13:16 +0900

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by koki on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
